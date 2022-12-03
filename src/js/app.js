a74f108bb337f7ed8e35331e9b5708f670d463b0
Atanas <atanasdraganov77@gmail.com>
2022-12-03 21:53:29 +0200

    fixed mistake with getting the price value

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,7 @@ window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   const products = document.querySelectorAll('.product');
   for(let i=0; i<products.length; i++){
-    products[i].setAttribute("data-price", "price")
-  }
 
+    products[i].setAttribute("data-price", $("#1").text())
+  }
 });
